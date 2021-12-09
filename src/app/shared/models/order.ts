43d3ca1610d377d83f64c330775fad24190f9138
AD\achervet2021 <annegaelle.chervet2021@campus-eni.fr>
2021-12-09 13:31:12 +0100

    Commit cless User et Order avec les attributs et les constructeurs

--- src/app/shared/models/order.ts
+++ src/app/shared/models/order.ts
@@ -1,2 +1,15 @@
+import {Pizza} from "./pizza";
+
 export class Order {
+  public id: number;
+  public date: Date = new Date();
+  public status: string;
+  public pizzasList: Pizza[]=[];
+
+  controller(id?: number, date?: Date, status?: string, pizzasList?: Pizza[]){
+    this.id = id;
+    this.date = date;
+    this.status = status;
+    this.pizzasList = pizzasList;
+  }
 }
--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,2 +1,13 @@
+import {Order} from "./order";
+
 export class User {
+  public id: number;
+  public name: string;
+  public order: Order;
+
+  constructeur(id?: number, name?: string, order?: Order){
+    this.id = id;
+    this.name = name;
+    this.order = order;
+  }
 }
