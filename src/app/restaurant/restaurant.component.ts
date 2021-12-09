c441d293f3fbbab164c63ae12c8e0868fdd73e64
AD\achervet2021 <annegaelle.chervet2021@campus-eni.fr>
2021-12-09 10:20:56 +0100

    Commit map ajouté sur la page restaurant

--- src/app/restaurant/restaurant.component.ts
+++ src/app/restaurant/restaurant.component.ts
@@ -13,3 +13,23 @@ export class RestaurantComponent implements OnInit {
   }
 
 }
+
+// Initialize and add the map
+/*function initMap(): void {
+  // The location of Eni
+  const eni = { lat: 48.1, lng: -1.700000 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 4,
+      center: eni,
+    }
+  );
+
+  // The marker, positioned at Eni
+  const marker = new google.maps.Marker({
+    position: eni,
+    map: map,
+  });
+}*/
